95e8f7c7e1f6dfdf4c1831de5cbdf6c338c33f20
Olacy Rodrigues da Silva junior <olacy_rodrigues@yahoo.com.br>
2021-04-06 13:17:00 -0300

    upload incompleto

---
+++ exercicios_alunos/Olacy Rodrigues/TaxiPoo/Carro.py
@@ -0,0 +1,18 @@
+class Carro:
+    
+ def __init__(self,placa,modelo,cor):
+        self._placa = placa
+        self.modelo = modelo
+        self.cor = cor
+
+def get_placa(self):
+        return self.placa
+def set_palca(self,placa):
+        return self.placa
+
+
+Carro1 = Carro("KBC9T63","370z - Nissan","Preto")
+
+print(Carro1)
+
+    
---
+++ exercicios_alunos/Olacy Rodrigues/TaxiPoo/Cliente.py
@@ -0,0 +1,14 @@
+from random import randint
+class Cliente:
+
+ def __init__(self,nome,codigo):
+        self.nome = nome
+        self._codigo = codigo
+
+def get_Codigo(self):
+        return self._codigo
+def set_Codigo(self,codigo):
+        codigo = randint(10000, 19999)
+        return self._codigo
+
+    
